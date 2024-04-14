768082a6f5e720bf2a434b77d13ee6e772019c2e
chlalszxc <162539461+chlalszxc@users.noreply.github.com>
2024-04-14 19:40:00 +0900

    Update 10차시.c

--- 10차시.c
+++ 10차시.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void example1(void);
+void example2(void);
 
 int main(void)
 {
@@ -9,36 +10,36 @@ int main(void)
 	int data1 = 0;
 	double data2 = 0.0;
 	char ch;
+
 	fp = fopen("data3.txt", "rt");
 
 	if (fp == NULL)
 	{
-		//printf("File writing Error!\n");
-		puts("File Reading Error!\n");
+		//printf("File Reading Error \n");
+		puts("File Reading Error \n");
 		return -1;
 	}
 	else
 	{
-		printf("File Reading Success!\n");
+		printf("File Reading Success \n");
 	}
-	for (i = 0;i < 2;i++)
+
+	for (i = 0; i < 2; i++)
 	{
 		//ch = fgetc(fp);
 		fscanf(fp, "%d", &data1);
-		printf("%d \n", data1);
-
+		printf("%d\n", data1);
 	}
-	for (i = 0;i < 2;i++)
+	for (i = 0; i < 2; i++)
 	{
 		//ch = fgetc(fp);
 		fscanf(fp, "%lf", &data2);
-		printf("%lf \n", data2);
-
+		printf("%lf\n", data2);
 	}
 	fclose(fp);
+
 	return 1;
 }
-
 
 void example1(void)
 {
@@ -48,49 +49,52 @@ void example1(void)
 
 	if (fp == NULL)
 	{
-		//printf("File writing Error!\n");
-		puts("File writing Error!\n");
+		//printf("File Writing Error \n");
+		puts("File Writing Error \n");
 		return;
 	}
 	else
 	{
-		printf("File Writing Success!\n");
+		printf("File Writing Success \n");
 	}
 
-	fputc('A', fp);  //fprintf(fp, "%c", 'A');
+	fputc('A', fp);
 	fputc('B', fp);
 	fputc('C', fp);
 	fputs("\nDEF\n", fp);
-	
+	//fprintf(fp, "A");
 	printf("%d\n", fclose(fp));
-	return;
 
+	return;
 }
 
 void example2(void)
 {
 	FILE* fp;
-	int i;
-	char ch;
+
+	int i = 0;
+	char ch = 0;
+
 	fp = fopen("data2.txt", "rt");
 
 	if (fp == NULL)
 	{
-		//printf("File writing Error!\n");
-		puts("File Reading Error!\n");
-		return;
+		//printf("File Reading Error \n");
+		puts("File Reading Error \n");
+		return ;
 	}
 	else
 	{
-		printf("File Reading Success!\n");
+		printf("File Reading Success \n");
 	}
-	for (i = 0;i < 7;i++)
+
+	for (i = 0; i <= 6; i++)
 	{
 		//ch = fgetc(fp);
 		fscanf(fp, "%c", &ch);
-		printf("%c \n", ch);
-
+		printf("%c\n", ch);
 	}
 	fclose(fp);
+
 	return;
 }
